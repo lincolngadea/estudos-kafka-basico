817dccf94f8d1b0fcd0e8a090cfdccd91134efc5
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-01 17:51:00 -0300

    chore: extrai uma camada de consumidor

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -6,31 +6,37 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain{
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "132123,67523,7894589745";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value,value);
 
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso enviando " + data.topic()
-                    + ":::partition " + data.partition()
-                    + "/ offset " + data.offset()
-                    + "/ timeStamp " + data.timestamp() + "/");
-        };
-        var email = "Obrigado por sua compra, nós estamos processando o seu pedido!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",email,email);
-        producer.send(record, callback).get();//o Send é um método assíncrono
+        for (var i = 0; i < 10; i++) {
 
-        producer.send(emailRecord, callback).get();
+            var key = UUID.randomUUID().toString();
+            var value = key + ",67523,7894589745";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("Sucesso enviando " + data.topic()
+                        + ":::partition " + data.partition()
+                        + "/ offset " + data.offset()
+                        + "/ timeStamp " + data.timestamp() + "/");
+            };
+            var email = "Obrigado por sua compra, nós estamos processando o seu pedido!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(record, callback).get();//o Send é um método assíncrono
+
+            producer.send(emailRecord, callback).get();
+        }
+
     }
 
     //Definindo as propriedades do kafka
